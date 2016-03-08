4bd6ec9f9aa04cc6d5e40d5b9bc77347feb19918
bro <bro@zengularity.com>
2016-03-08 11:57:14 +0100

    vue login et sign up

---
+++ inscription.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hackathon</title>
+		<link rel="stylesheet" href="css/styles.css" />
+		<script src="modules/jquery-1.11.3.min.js"></script>
+	</head>
+	<body>
+		
+	<div id="inscription">
+		<h1>Inscription</h1>
+
+		<input name="email" type="text" placeholder="email">
+		<input name="password" type="password">
+		<button id="inscription">inscription</button>
+	</div>
+
+	</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hackathon</title>
+		<link rel="stylesheet" href="css/styles.css" />
+		<script src="modules/jquery-1.11.3.min.js"></script>
+	</head>
+	<body>
+		
+	<div id="login">
+		<h1>Login</h1>
+
+		<input name="login" type="text" placeholder="Id client ou email">
+		<input name="password" type="password">
+		<button id="connexion">Connexion</button>
+
+	</div>
+
+	</body>
+
+</html>
