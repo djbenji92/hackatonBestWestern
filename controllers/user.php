3bbba9656e0bbae9f55f92328378bd901f81bfd8
bro <bro@zengularity.com>
2016-03-08 22:33:29 +0100

    fix inscription

--- controllers/user.php
+++ controllers/user.php
@@ -1,32 +1,32 @@
 <script>
 
 $("#inscription").click(function(){
-			var username = $("#username").val();
-            var password = $("#password").val();
-            var random = parseInt(Math.random() * 10000000000);
+	var username = $("#username").val();
+    var password = $("#password").val();
+    var random = parseInt(Math.random() * 10000000000);
 		
-			  //AJAX pour envoyer valeur dans base de données
-			  var xhr;
-				try { xhr = new XMLHttpRequest(); }
-				catch (e) {
-				xhr = new ActiveXObject(Microsoft.XMLHTTP);
-							 }  
-				 xhr.onreadystatechange = function () {
-				if (xhr.readyState == 4)
-				if (xhr.status == 200){
-					//alert('ok');
-					console.log(xhr.responseText);
-					
-				}
-				//document.getElementById("mydiv").innerHTML=xhr.responseText;
-						else
-				alert(xhr.status);
-				}
+	//AJAX pour envoyer valeur dans base de données
+	var xhr;
+	try { 
+		xhr = new XMLHttpRequest(); 
+	}
+	catch (e) {
+		xhr = new ActiveXObject(Microsoft.XMLHTTP);
+	}  
+	xhr.onreadystatechange = function () {
+	if (xhr.readyState == 4)
+		if (xhr.status == 200){
+			//alert('ok');
+			console.log(xhr.responseText);	
+		}
+		//document.getElementById("mydiv").innerHTML=xhr.responseText;
+		else
+			alert(xhr.status);
+		}
 
-				xhr.open('POST', 'modele/inscription.php', true);
-				xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-				xhr.send('username=' + username + '&password=' + password +'&random=' + random);
-
+		xhr.open('POST', 'modele/inscription.php', true);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.send('username=' + username + '&password=' + password +'&random=' + random);
 });
 
 </script>
