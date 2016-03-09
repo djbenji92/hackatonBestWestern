69db32c4ad90ae73dbce98d960b5fc924c11beaf
bro <bro@zengularity.com>
2016-03-09 09:04:28 +0100

    liste hotel

---
+++ listeHotel.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hackathon</title>
+		<link rel="stylesheet" href="css/styles.css" />
+		<script src="modules/jquery-1.11.3.min.js"></script>
+	</head>
+	<body>
+	<?php include("controllers/hotel.php"); ?>
+
+	<div id="Liste hotel">
+		<h1>Liste des hotels</h1>
+		<?php listeHotel(); ?>
+		
+
+	</div>
+
+	
+	</body>
+
+</html>
